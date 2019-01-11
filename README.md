9c551293b2b87e8ce54b4dfab5f2e82714a318ce
Dennis Wilson <dennis@d9w.xyz>
2019-01-11 20:09:05 +0100

    2019 second lecture

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# computational_intelligence
-CI M2 course
+# Computational Intelligence
+
+Course materials for the Computational Intelligence class in the 2IS Master
+program at University of Toulouse 1 Capitole. More information about the
+Master's program can be found here:
++ http://miage.ut-capitole.fr/2is
++ https://www.dailymotion.com/video/x6hjk9q
+
+This class is comprised of two sections, the first on machine learning and the
+second on reasoning. This repository contains the materials for the first
+section on machine learning.
+
+Notebooks are inspired by, taken from, or reference a variety of sources,
+including but not limited to:
++ https://github.com/jrjohansson/scientific-python-lectures
++ https://scikit-learn.org/stable/user_guide.html
++ https://scikit-learn.org/stable/auto_examples/index.html
++ https://github.com/justmarkham/scikit-learn-videos
++ https://github.com/pnavaro/python-notebooks
--- notebooks/exercises/02_diabetes_knn.py
+++ notebooks/exercises/02_diabetes_knn.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 diabetes = load_diabetes()
 
-k = 4
+k = 10
 
 X_train, X_test, y_train, y_test = train_test_split(diabetes.data, diabetes.target,
                                                     test_size=0.25)
@@ -19,14 +19,16 @@ print('k: %d, Train Acc: %.3f, Test Acc: %.3f' % (k, train_score, test_score))
 y_pred_train = knn.predict(X_train)
 y_pred_test = knn.predict(X_test)
 
-x_axis = 0
+x_axis = 3
 
-plt.figure(figsize=(20,10))
+plt.figure(figsize=(20, 5))
 plt.subplot(1, 2, 1)
 plt.plot(X_train[:, x_axis], y_train, 'o', label="data", markersize=10)
-plt.plot(X_train[: x_axis], y_pred_train, 's', label="prediction", markersize=4)
+plt.plot(X_train[:, x_axis], y_pred_train, 's', label="prediction", markersize=4)
 plt.title("KNN [train]")
 plt.subplot(1, 2, 2)
 plt.plot(X_test[:, x_axis], y_test, 'o', label="data", markersize=10)
-plt.plot(X_test[: x_axis], y_pred_test, 's', label="prediction", markersize=4)
+plt.plot(X_test[:, x_axis], y_pred_test, 's', label="prediction", markersize=4)
 plt.title("KNN [test]");
+
+
